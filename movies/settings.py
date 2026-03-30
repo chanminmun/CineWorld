af0bfa0caa0749edb5c5741fa6cf074f35cdb6d0
mun <mchanmin0801@gmail.com>
2026-03-30 10:09:47 +0900

    allowed error

--- movies/settings.py
+++ movies/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = [
-    host.strip()
-    for host in os.environ.get("ALLOWED_HOSTS", "").split(",")
-    if host.strip()
-]
+ALLOWED_HOSTS = [h.strip() for h in os.getenv("ALLOWED_HOSTS", "").split(",") if h.strip()]
 
 
 # Application definition
